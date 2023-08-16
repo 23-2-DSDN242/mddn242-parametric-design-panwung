18b6f7ed7ad2d5263a42a57115a7b23a3204c3c9
panwung <panwung@myvuw.ac.nz>
2023-08-17 10:22:01 +1200

    Update letters.js

--- letters.js
+++ letters.js
@@ -1,188 +1,101 @@
-const alphabet = {
-  "default": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 0
-  },
-  "A": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "B": {
-    "size": 75,
-    "offsetx": 0,
-    "offsety": -70
-  },
-  "C": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "D": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "E": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "F": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "G": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "H": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "I": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "J": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "K": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "L": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "M": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "N": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "O": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "P": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "Q": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "R": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "S": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "T": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "U": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "V": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "W": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "X": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "Y": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "Z": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
-  },
-  "0": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "1": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "2": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "3": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "4": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "5": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "6": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "7": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "8": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
-  },
-  "9": {
-    "size": 40,
-    "offsetx": 0,
-    "offsety": 17
+class Character {
+  constructor(size, offsetx, offsety) {
+    this.size = size;
+    this.offsetx = offsetx;
+    this.offsety = offsety;
   }
+}
 
+const alphabet = {
+  "default": new Character(40, 0, 0),
+  "A": new Character(40, 0, 17),
+  "B": new Character(75, 0, -70),
+  "C": new Character(50, 15, 0),
+  "D": new Character(50, 15, 0),
+  "E": new Character(50, 15, 0),
+  "F": new Character(50, 15, 0),
+  "G": new Character(50, 15, 0),
+  "H": new Character(50, 15, 0),
+  "I": new Character(50, 15, 0),
+  "J": new Character(
+    50, 15, 0
+  ),
+  "K": new Character(
+    50, 15, 0
+  ),
+  "L": new Character(
+    50, 15, 0
+  ),
+  "M": new Character(
+    50, 15, 0
+  ),
+  "N": new Character(
+    50, 15, 0
+  ),
+  "O": new Character(
+    50, 15, 0
+  ),
+  "P": new Character(
+    50, 15, 0
+  ),
+  "Q": new Character(
+    50, 15, 0
+  ),
+  "R": new Character(
+    50, 15, 0
+  ),
+  "S": new Character(
+    50, 15, 0
+  ),
+  "T": new Character(
+    50, 15, 0
+  ),
+  "U": new Character(
+    50, 15, 0
+  ),
+  "V": new Character(
+    50, 15, 0
+  ),
+  "W": new Character(
+    50, 15, 0
+  ),
+  "X": new Character(
+    50, 15, 0
+  ),
+  "Y": new Character(
+    50, 15, 0
+  ),
+  "Z": new Character(
+    50, 15, 0
+  ),
+  "0": new Character(
+    40, 0, 17
+  ),
+  "1": new Character(
+    40, 0, 17
+  ),
+  "2": new Character(
+    40, 0, 17
+  ),
+  "3": new Character(
+    40, 0, 17
+  ),
+  "4": new Character(
+    40, 0, 17
+  ),
+  "5": new Character(
+    40, 0, 17
+  ),
+  "6": new Character(
+    40, 0, 17
+  ),
+  "7": new Character(
+    40, 0, 17
+  ),
+  "8": new Character(
+    40, 0, 17
+  ),
+  "9": new Character(
+    40, 0, 17
+  )
 }
