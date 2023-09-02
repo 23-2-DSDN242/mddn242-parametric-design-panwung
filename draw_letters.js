3d87db8a9d98002b5152acb279447f197b3f307e
panwung <panwung@myvuw.ac.nz>
2023-09-02 21:24:57 +1200

    Update draw_letters.js
    made some changes to interpolation function

--- draw_letters.js
+++ draw_letters.js
@@ -20,31 +20,38 @@ function drawLetter(letterData) {
 
 function interpolate_letter(percent, oldObj, newObj) {
   let newLetter = {};
-  
+
   newLetter.offsetx1     = map(percent, 0, 100, oldObj.offsetx1, newObj.offsetx1);
   newLetter.offsety1     = map(percent, 0, 100, oldObj.offsety1, newObj.offsety1);
-  newLetter.radius1      = map(percent, 0, 100, oldObj.radius1, newObj.radius1);
-  newLetter.arcStart1    = map(percent, 0, 100, oldObj.arcStart1, newObj.arcStart1);
-  newLetter.arcStop1     = map(percent, 0, 100, oldObj.arcStop1, newObj.arcStop1);
+  newLetter.radius1      = map((2 * percent > 100) ? 100 : 2 * percent, 0, 100, oldObj.radius1, newObj.radius1);
+
+  newLetter.arcStart1    = oldObj.arcStart1 + percent * (newObj.arcStart1 - oldObj.arcStart1) / 100;
+  newLetter.arcStop1     = oldObj.arcStop1 + percent * (newObj.arcStop1 - oldObj.arcStop1) / 100;
 
   newLetter.offsetx2     = map(percent, 0, 100, oldObj.offsetx2, newObj.offsetx2);
   newLetter.offsety2     = map(percent, 0, 100, oldObj.offsety2, newObj.offsety2);
-  newLetter.radius2      = map(percent, 0, 100, oldObj.radius2, newObj.radius2);
-  newLetter.arcStart2    = map(percent, 0, 100, oldObj.arcStart2, newObj.arcStart2);
-  newLetter.arcStop2     = map(percent, 0, 100, oldObj.arcStop2, newObj.arcStop2);
+  newLetter.radius2      = map((2 * percent > 100) ? 100 : 2 * percent, 0, 100, oldObj.radius2, newObj.radius2);
 
-  newLetter.lineXCenter  = map(percent, 0, 100, oldObj.lineXCenter, newObj.lineXCenter);
-  newLetter.lineYCenter  = map(percent, 0, 100, oldObj.lineYCenter, newObj.lineYCenter);
+  newLetter.arcStart2    = oldObj.arcStart2 + ((percent < 50) ? 0 : 2 * (percent - 50)) * (newObj.arcStart2 - oldObj.arcStart2) / 100;
+  newLetter.arcStop2     = oldObj.arcStop2 + ((percent < 50) ? 0 : 2 * (percent - 50)) * (newObj.arcStop2 - oldObj.arcStop2) / 100;
+
+  newLetter.lineXCenter  = map((2 * percent > 99) ? 100 : 2 * percent, 0, 100, oldObj.lineXCenter, newObj.lineXCenter);
+  newLetter.lineYCenter  = map((percent < 50) ? 0 : 2 * (percent - 50), 0, 100, oldObj.lineYCenter, newObj.lineYCenter);
   newLetter.lineLength   = map(percent, 0, 100, oldObj.lineLength, newObj.lineLength);
   newLetter.lineRotation = map(percent, 0, 100, oldObj.lineRotation, newObj.lineRotation);
 
-  newLetter.draw = oldObj.draw
+  newLetter.draw = oldObj.draw;
+
 
   return newLetter;
+
+
 }
+
 
 var swapWords = [
   "ABBAABBA",
   "CAB?CAB?",
-  "BAAAAAAA"
+  "BAAAAAAA",
+  "AMBASING",
 ]
